76a82f927c2ddc0bb96bec08da2967ae171eb968
Aniket Habib <had096705@gmail.com>
2020-07-17 20:18:27 +0530

    Updates

---
+++ precache-manifest.1a9b7995c57ce8d63421d2e0569d2265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c63d382faaa01d4d8684d574ed8c6a7",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "dcabcec580dba7055dcb",
+    "url": "/covid-tracker/static/css/main.5d895242.chunk.css"
+  },
+  {
+    "revision": "3dd11840010a69609ddc",
+    "url": "/covid-tracker/static/js/2.31da174b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.31da174b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcabcec580dba7055dcb",
+    "url": "/covid-tracker/static/js/main.05c3d92c.chunk.js"
+  },
+  {
+    "revision": "f210d2661cd825de531c",
+    "url": "/covid-tracker/static/js/runtime-main.b82cc920.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
